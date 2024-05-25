47f069d1b3d7b404fc44e8a322c2271f61ac3dfb
x1zy <nikitagusev672@gmail.com>
2024-05-25 21:19:55 +0300

    Финальное ревью

---
+++ app/api/config.js
@@ -0,0 +1,7 @@
+const BASE_URL = "https://mybackend.nomoredomainswork.ru/api";
+
+export const endpoints = {
+  games: `${BASE_URL}/games`,
+  auth: `${BASE_URL}/auth/login`,
+  me: `${BASE_URL}/me`,
+};
